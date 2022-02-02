54ef17fe1d1ac67a7a95df7669f53c21318a4a06
Ariadna S <iamariadnaayala@gmail.com>
2022-02-02 14:35:47 -0600

    Delete main.cc

--- main.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    //How Many SWEs do you need to change a light bulb?
-    cout << " None. This is a hardware problem. Call the technician, we have bugs to debug.\n ";
-    return 0;
-}
