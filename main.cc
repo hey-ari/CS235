b60d4972791a896648e9026e4364005ae80ff0d9
Ariadna S <iamariadnaayala@gmail.com>
2022-02-02 14:34:57 -0600

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //How Many SWEs do you need to change a light bulb?
+    cout << " None. This is a hardware problem. Call the technician, we have bugs to debug.\n ";
+    return 0;
+}
