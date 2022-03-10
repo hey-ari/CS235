d5893f3a9f5f0b9af60a5c70d89cd1aa43311e5a
Ariadna S <iamariadnaayala@gmail.com>
2022-03-10 13:12:23 -0600

    Create exp6.cc

---
+++ Valgrind_Lab/exp6.cc
@@ -0,0 +1,10 @@
+#include <cstring>		// C-strings: for strcpy()
+using namespace std;
+
+int main() {
+    char *greeting = new char[1024];
+    strcpy(greeting, "Dear");
+    delete [] greeting;
+    cout << greeting << " Sir/Madam\n";
+    return 0;
+}
